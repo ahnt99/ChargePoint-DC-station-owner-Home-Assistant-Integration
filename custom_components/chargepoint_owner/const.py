283ed00a0d5175d3ba97cac14fb64f9ce940dbcd
ahnt99 <112784531+ahnt99@users.noreply.github.com>
2026-09-19 13:44:29 -0400

    Add files via upload

--- custom_components/chargepoint_owner/const.py
+++ custom_components/chargepoint_owner/const.py
@@ -16,8 +16,8 @@ DEFAULT_NAME = "ChargePoint"
 import pathlib
 
 _DIR = pathlib.Path(__file__).parent
-CHARGEPOINT_WSDL = str(_DIR / "chargepoint_5.0.wsdl")
-CHARGEPOINT_ENDPOINT = "https://webservices.chargepoint.com/webservices/chargepoint/services/5.0"
+CHARGEPOINT_WSDL = str(_DIR / "chargepoint_5.1.wsdl")
+CHARGEPOINT_ENDPOINT = "https://webservices.chargepoint.com/webservices/chargepoint/services/5.1"
 WSSE_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
 PASSWORD_TYPE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText"
 
